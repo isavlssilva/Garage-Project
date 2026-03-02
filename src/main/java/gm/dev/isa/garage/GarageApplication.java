72cd250b695a5fe08ea146f55146841ac360d67b
isa <isabellevitorialimadasilva@gmail.com>
2026-03-02 16:59:32 -0300

    Commit inicial V 1.0

---
+++ src/main/java/gm/dev/isa/garage/GarageApplication.java
@@ -0,0 +1,13 @@
+package gm.dev.isa.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageApplication.class, args);
+	}
+
+}
