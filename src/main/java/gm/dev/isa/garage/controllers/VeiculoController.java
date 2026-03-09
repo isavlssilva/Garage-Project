f1657e7abb43315d1dac635398e3522315fb2b17
isa <isabellevitorialimadasilva@gmail.com>
2026-03-09 14:11:59 -0300

    Término da Fase 2 –Implementado endpoint forsale -Add interface VeiculoRepository -Add classe VeiculoService -Add classe VeiculoController

--- src/main/java/gm/dev/isa/garage/controllers/VeiculoController.java
+++ src/main/java/gm/dev/isa/garage/controllers/VeiculoController.java
@@ -4,10 +4,29 @@
  */
 package gm.dev.isa.garage.controllers;
 
+import gm.dev.isa.garage.entities.Veiculo;
+import gm.dev.isa.garage.service.VeiculoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author digma
  */
+
+@RestController
 public class VeiculoController {
+    
+    @Autowired
+    private VeiculoService veiculoService;
+    
+    @GetMapping("/forsale")
+    public List<Veiculo> findAll(){
+        List<Veiculo> result = veiculoService.findAll();
+        return result;
+        
+    }
     
 }
